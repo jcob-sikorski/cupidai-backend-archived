c6fd320d08e9f5b86caa90927c038377288a0794
jcob-sikorski <jm.siekiera@gmail.com>
2024-04-15 21:50:43 +0200

    m

--- src/service/account.py
+++ src/service/account.py
@@ -33,21 +33,28 @@ def get_password_hash(password):
 
 def authenticate_user(username: str, password: str):
     user = data.get_user(username)
+
     if not user:
         return False
+    
     if not verify_password(password, user.hashed_password):
         return False
+    
     return user
 
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
+
     if expires_delta:
         expire = datetime.now(timezone.utc) + expires_delta
     else:
         expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+
     to_encode.update({"exp": expire})
+
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
     return encoded_jwt
 
 
@@ -57,17 +64,23 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
+
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
+
         if username is None:
             raise credentials_exception
+        
         token_data = TokenData(username=username)
     except JWTError:
         raise credentials_exception
+    
     user = data.get_user(username=token_data.username)
+
     if user is None:
         raise credentials_exception
+    
     return user
 
 
@@ -76,26 +89,28 @@ async def get_current_active_user(
 ):
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
+    
     return current_user
 
-async def login( # TODO: this annotated should be probably not have injection there
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-) -> Token:
+async def login(form_data: OAuth2PasswordRequestForm) -> Token:
     user = authenticate_user(form_data.username, form_data.password)
+
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
+    
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
+
     return Token(access_token=access_token, token_type="bearer")
 
 
-def signup(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> None:
+def signup(form_data: OAuth2PasswordRequestForm) -> None:
     data.signup(form_data.username, form_data.password)
     
     return login(form_data.username, form_data.password)
