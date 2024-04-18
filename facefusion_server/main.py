6bfb18a7bc056c1c420bd3c779dc7ea3864eca03
jcob-sikorski <jm.siekiera@gmail.com>
2024-04-18 22:04:55 +0200

    m

---
+++ facefusion_server/main.py
@@ -0,0 +1,208 @@
+#This is an example that uses the websockets api to know when a prompt execution is done
+#Once the prompt execution is done it downloads the images using the /history endpoint
+
+import uuid
+import os
+import httpx
+import requests
+import cv2
+import numpy as np
+
+import subprocess
+
+from dotenv import load_dotenv
+
+from fastapi import FastAPI, Request
+import uvicorn
+
+import boto3
+
+from pydantic import BaseModel
+
+from typing import List, Dict, Optional
+
+load_dotenv()
+
+server_address = "127.0.0.1:8188"
+client_id = str(uuid.uuid4())
+
+app = FastAPI()
+
+# Load AWS S3 Access keys from environment variables
+S3_ACCESS_KEY = os.getenv('S3_ACCESS_KEY')
+S3_SECRET_ACCESS_KEY = os.getenv('S3_SECRET_ACCESS_KEY')
+
+boto3.setup_default_session(aws_access_key_id=S3_ACCESS_KEY,
+                            aws_secret_access_key=S3_SECRET_ACCESS_KEY,
+                            region_name='us-east-1')
+
+def upload_image_to_s3(output_path):
+    s3_client = boto3.client('s3')
+
+    # Read the image file in binary mode
+    with open(output_path, 'rb') as f:
+        image_data = f.read()
+
+    # Convert the binary data to a numpy array
+    image_array = np.frombuffer(image_data, np.uint8)
+
+    # Decode the image data
+    image = cv2.imdecode(image_array, cv2.IMREAD_UNCHANGED)
+
+    # Save the decoded image as a PNG file
+    cv2.imwrite(output_path, image)
+
+    with open(output_path, 'rb') as data:
+        s3_client.upload_fileobj(data, 'magicalcurie', output_path)
+
+    s3_uri = f's3://magicalcurie/{output_path}'
+
+    # Remove the local image file
+    os.remove(output_path)
+
+    return s3_uri
+
+def download_and_save_images(uploadcare_uris: Dict[str, str], image_ids: Dict[str, str], predefined_path: str) -> None:
+    """
+    Downloads and saves images based on image URIs and image IDs.
+    Args:
+        uploadcare_uris (Dict[str, str]): Dictionary of image URIs.
+        image_ids (Dict[str, str]): Dictionary of image IDs.
+        predefined_path (str): Predefined path to save the images.
+    """
+    print(f"DOWNLOADING AND SAVING IMAGES")
+    for key, uri in uploadcare_uris.items():
+        print(f"GETTING IMAGE ID")
+        image_id = image_ids.get(key)
+        
+        if image_id:
+            print(f"SETTING THE UNIQUE FILEPATH OF THE IMAGE")
+            image_path = os.path.join(predefined_path, f"{image_id}.jpg")
+            print(f"DOWNLOADING IMAGE")
+            response = requests.get(uri)
+            if response.status_code == 200:
+                print(f"WRITING IMAGE TO THE UNIQUE PATH")
+                with open(image_path, "wb") as f:
+                    f.write(response.content)
+                print(f"Image saved at {image_path}")
+            else:
+                print(f"Failed to download image from {uri}")
+        else:
+            print(f"No image ID found for key: {key}")
+
+def remove_images(image_ids: Dict[str, str], predefined_path: str) -> None:
+    """
+    Removes images based on image IDs.
+    Args:
+        image_ids (Dict[str, str]): Dictionary of image IDs.
+        predefined_path (str): Predefined path where the images are stored.
+    """
+    print(f"REMOVING IMAGES")
+    for key, image_id in image_ids.items():
+        print(f"SETTING THE UNIQUE FILEPATH OF THE IMAGE")
+        image_path = os.path.join(predefined_path, f"{image_id}.jpg")
+        try:
+            print(f"REMOVING AN IMAGE")
+            os.remove(image_path)
+            print(f"Image removed: {image_path}")
+        except FileNotFoundError:
+            print(f"Image not found: {image_path}")
+
+class Message(BaseModel):
+    user_id: Optional[str] = None
+    status: Optional[str] = None
+    uploadcare_uris: Optional[Dict[str, str]] = None
+    created_at: Optional[str] = None
+    message_id: Optional[str] = None
+    s3_uri: str = None
+
+async def send_webhook_acknowledgment(user_id: str, message_id: str, status: str, webhook_url: str, s3_uri: str = None) -> None:
+    """
+    Sends an acknowledgment message via webhook.
+
+    Args:
+        user_id (str): The unique identifier for the user.
+        message_id (str): The unique identifier for the message.
+        status (str): The status of the message.
+        webhook_url (str): The URL of the webhook endpoint.
+        s3_uri (str): The S3 URI associated with the message.
+
+    Returns:
+        None
+    """
+    print("SENDING WEBHOOK ACKNOWLEDGMENT")
+    try:
+        print("CREATING DICTIONARY TO STORE THE FIELDS")
+        # Create a dictionary to store the fields
+        message_fields = {
+            'user_id': user_id,
+            'message_id': message_id,
+            'status': status
+        }
+
+        if s3_uri is not None:
+            print("ADDING S3_URI FIELD TO MESSAGE MODEL")
+            message_fields['s3_uri'] = s3_uri
+
+        print("CREATING MESSAGE MODEL")
+        # Create the Message object
+        message = Message(**message_fields)
+
+        print(f"MAKING POST REQUEST TO THE WEBHHOK {webhook_url}")
+        async with httpx.AsyncClient() as client:
+            response = await client.post(webhook_url, json=message.__dict__)
+            if response.status_code == 200:
+                print("Webhook request successful!")
+            else:
+                print(f"Webhook request failed with status code {response.status_code}")
+    except Exception as e:
+        print(f"Error sending acknowledgment: {str(e)}")
+
+@app.post("/")
+async def create_item(request: Request):
+    payload = await request.json()
+    uploadcare_uris = payload.get('uploadcare_uris', {})
+    source_ids = payload.get('source_ids', {})
+    target_id = payload.get('target_id', {})
+    message_id = payload.get('message_id', {})
+    user_id = payload.get('user_id', {})
+
+    webhook_url = 'https://garfish-cute-typically.ngrok-free.app/image-generation/webhook'
+
+    await send_webhook_acknowledgment(user_id, message_id, 'in progress', webhook_url)
+
+    try:
+        predefined_path = 'C:\\Users\\Shadow\\Desktop'
+
+        output_id = uuid.uuid4()
+
+        image_ids = source_ids + [target_id, output_id]
+
+        download_and_save_images(uploadcare_uris, image_ids, predefined_path)
+
+        target_path = predefined_path + f"{target_id}.png"
+        output_path = os.path.join(predefined_path, f"{output_id}.png")
+
+        command = ["python", "run.py", "--headless", "-t", target_path, "-o", output_path]
+        
+        # Add a '-s' flag for each image
+        for source_id in source_ids:
+            source_path = os.path.join(predefined_path, f"{source_id}.jpg")
+            command.extend(["-s", source_path])
+        
+        # Run the command
+        subprocess.run(command, check=True)
+
+        s3_uri = upload_image_to_s3(output_path)
+    except Exception as e:
+        await send_webhook_acknowledgment(user_id, message_id, 'failed', webhook_url)
+
+    await send_webhook_acknowledgment(user_id, message_id, 'completed', webhook_url, s3_uri)
+
+    remove_images(image_ids, predefined_path)
+
+    return s3_uri
+
+# Run the server
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
